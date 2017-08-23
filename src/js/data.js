427db4940335e24b469d39c847e3957c381d7b3b
Ezra <ezrazigmond@gmail.com>
2017-08-23 12:18:01 -0400

    Fixed typos

--- src/js/data.js
+++ src/js/data.js
@@ -66,8 +66,8 @@ exports.teaching = [
         timeline: "Fall 2015",
         description: undefined,
         details: [
-            "Led a weekly section for 20 students and grade problem sets.",
-            "Held office hours and fielded questions via email and the course discussion site.",
+            "Led a weekly section for 20 students and graded problem sets.",
+            "Held office hours and answeredf questions via email and the course discussion site.",
             "Advised groups of students working on final projects.",
         ]
     }
@@ -152,7 +152,7 @@ exports.experiences = [
                 "to identify document borders in images.",
             "Used OpenGL ES to allow users to crop, de-skew, and adjust contrast/brightness levels of captured images.",
             "Integrated mobile app with server-side optical character recognition (OCR) engine so mobile users could upload documents for text recognition.",
-            "Integrated client-side OCR with <a href=\"https://code.google.com/p/tesseract-ocr/\">Tesseract</a> to compare the experience with server-side OCR."
+            "Integrated app with client-side OCR using <a href=\"https://code.google.com/p/tesseract-ocr/\">Tesseract</a> to compare the experience with server-side OCR."
         ]
     },
     {
@@ -164,7 +164,7 @@ exports.experiences = [
         employer: "Digital Atlas of Roman and Medieval Civilization (DARMC)",
         title: "Undergraduate Research Assistant",
         timeline: "2015",
-        description: "DARMC is a digital humanities project at Harvard that pubishes freely available maps and geodatabases created from " +
+        description: "DARMC is a digital humanities project at Harvard that publishes freely available maps and geodatabases created from " +
             "historical data on Roman and medieval civilization. A common task in the project was manually looking up and filling in " +
             "coordinates for large CSV files of historical place names. I improved on an existing effort to automate this process.",
         details: [
@@ -196,7 +196,7 @@ exports.projects = [
             "Techniques from language-based security, such as security type systems, often provide protection against high-level adversaries " +
             "but cannot guarantee security against low-level attackers, such as those that can inject code or inspect memory.</p>" +
             "<p>We model in <a href=\"https://coq.inria.fr/\">Coq</a> a security-typed calculus that includes an abstract model of Intel SGX-like enclaves, and show that the calculus provides " +
-            "meaningful security guarantees, even in the face of lowlevel attackers. We also model a translation scheme from an enclave-agnostic calculus " +
+            "meaningful security guarantees, even in the face of low-level attackers. We also model a translation scheme from an enclave-agnostic calculus " +
             "to an enclave-aware calculus and show that the scheme ensures that enclaves are correctly placed, so that a translation of a well-typed program " +
             "in the enclave-agnostic calculus results in a well-typed program in the enclave-aware calculus.",
         images: []
@@ -219,12 +219,12 @@ exports.projects = [
             }
         ],
         description: "<p>A final project for System Security at Harvard.</p>" +
-            "<p>To prevent automated systems from accessing certain webpages pages, website developers often add techniques to verify that a user is a human. " +
-            "These techniques generally involve tasks that are relatively easy for humans but difficult for automated systems. " +
+            "<p>To prevent automated systems from accessing certain webpages, website developers often wish to verify that a user is a human. " +
+            "This generally involves asking a user to perform tasks that are relatively easy for humans but difficult for automated systems. " +
             "One of the most popular such methods is CAPTCHA. However, these tasks can still be difficult or unpleasant to complete for humans. " +
             "In general, they can reduce the quality of user experience, which may make a user frustrated when interacting with a system, particularly for those with disabilities.</p>" +
-            "<p>To address these issues, we explore a replacement for CAPTCHA that uses a mobile client’s motion data to determine whether the client is an automated system. " +
-            "We also explore the privacy implications of websites clandestinely collecting accelerometer data.</p>",
+            "<p>To address these issues, we explore a replacement for CAPTCHA that uses a mobile client’s accelerometer data to determine whether the client is an automated system. " +
+            "We also examine the privacy implications of websites clandestinely collecting accelerometer data.</p>",
         images: [
             {
                 name: "captcha.png",
@@ -277,13 +277,13 @@ exports.projects = [
                 url: "https://github.com/ezig/cs50-final"
             }
         ],
-        description: "<p>A polite app to streamline your budget management experience. Available free on the app store. " +
+        description: "<p>A polite app to streamline your budget management experience. " +
             "Budget Butler is designed to simplify keeping track of receipts and spending by making it easy " +
             "to add new receipts as you get them and to view past spending in table or chart form.</p>" +
             "<p>Adding a picture of a new receipt automatically fills in the total amount using optical character " +
             "recognition (OCR) powered by <a href=\"https://code.google.com/p/tesseract-ocr/\">Tesseract</a>. Receipt pictures " +
             "and information are then saved using Core Data. Budget Butler will even autocomplete payee names and expense categories " +
-            "based on previous saved purchases. In addition to the table-style log, the summary view generates a pie chart using " +
+            "based on previous saved purchases. In addition to the table-style log, the summary view generates charts using " +
             "<a href=\"https://github.com/core-plot/core-plot\">Core Plot</a> to break down spending by category.</p>",
         images: [
             {
@@ -315,7 +315,7 @@ exports.projects = [
                 url: "https://ezig.github.io/npuzzles.pdf"
             }
         ],
-        description: "<p>A sliding tile puzzles solver written in Clojure as a final project for CS51: Abstraction and Design. " +
+        description: "<p>A sliding tile puzzle solver written in Clojure as a final project for CS51: Abstraction and Design. " +
             "The goal of the project was to compare the effectiveness of a standard A* strategy with a genetic algorithm. " +
             "After running tests on both square and non-square puzzles, we found our A* implementation about 30 times faster " +
             "than the genetic algorithm. We also compared the effectiveness of different heuristic functions for maximizing the " +
@@ -351,13 +351,13 @@ exports.projects = [
                 url: "https://github.com/ezig/strategy-game"
             }
         ],
-        description: "<p>An experiment with path-finding similar modeled on the way that movement works in turn-based strategy games " +
-            "like Fire Emblem and Advance Wars. Determines valid movement range based on a character's movement range " +
-            "and finds paths to tiles using a modified depth-limited breadth-first search that finds the shortest path to each tile. " +
+        description: "<p>An experiment with path-finding similar based on the style of movement found in turn-based strategy games " +
+            "like Fire Emblem and Advance Wars. Determines valid movement ranges and paths based on a character's movement range " +
+            "and finds paths to tiles using a modified depth-limited breadth-first search. " +
             "Built using <a href=\"http://phaser.io/\">Phaser.js</a> to draw grid and animate movement.</p>" +
             "<p>To try out the demo, click on and move the pink or the green friendly sprites to a valid (blue-highlighted) " +
             "tile in movement range. Once you've moved both friendly sprites, the red enemy will make a move. " +
-            "The red-highlighteds squares indicate spaces within the sprite's \"attack\" range but outside of its move range.</p>",
+            "The red-highlighted squares indicate spaces within the sprite's \"attack\" range but outside of its move range.</p>",
         images: [
             {
                 name: "strategy.png",
@@ -388,7 +388,7 @@ exports.projects = [
             "<p>There are currently two different modes:" +
             "<ul>" +
             "<li>Classic: randomly generate square levels from 3x3 up to 5x5</li>" +
-            "<li>Adventure: hand-made levels of varying shape (shown on the right)</li>" +
+            "<li>Adventure: handmade levels of varying shape (shown on the right)</li>" +
             "</ul></p>",
         images: [
             {
